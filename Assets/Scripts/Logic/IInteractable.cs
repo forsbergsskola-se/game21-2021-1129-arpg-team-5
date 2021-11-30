b760fec3be1c2d019d34660bfa80d3211681a3d5
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-30 14:14:36 +0100

    Interactable.cs :  Optimised code
    Removed "MouseController mouseController" parameters for the interface methods.

--- Assets/Scripts/Logic/IInteractable.cs
+++ Assets/Scripts/Logic/IInteractable.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using Entities.Player;
 using UnityEngine;
 
-public interface IInteractable
+namespace Logic
 {
-    
-    public Texture2D mouseTexture { get; }
-    /// <summary>
-    /// Implement your OnHover method. If the mouse sprite should change, access a sprite variable in the MouseControl script. LAST PART NOT IMPLEMENTED YET, MIGHT CHANGE!
-    /// </summary>
-    public void OnHover(MouseController mouseController);
+    public interface IInteractable
+    {
+        /// <summary>
+        /// The texture to change the mouse to. Modify the get to return the value of a public field if you want it to change, or leave it as is if the mouse should be the default look.
+        /// </summary>
+        public Texture2D mouseTexture { get; }
 
-    /// <summary>
-    /// Implement your OnClick method. This is called if your object is clicked, so implement any logic that should happen if a object is clicked here.
-    /// </summary>
-    public void OnClick(MouseController mouseController);
+        /// <summary>
+        /// Implement your OnHover method. If the mouse sprite should change, access a sprite variable in the MouseControl script. LAST PART NOT IMPLEMENTED YET, MIGHT CHANGE!
+        /// </summary>
+        public void OnHover();
+
+        /// <summary>
+        /// Implement your OnClick method. This is called if your object is clicked, so implement any logic that should happen if a object is clicked here.
+        /// </summary>
+        public void OnClick();
+    }
 }
