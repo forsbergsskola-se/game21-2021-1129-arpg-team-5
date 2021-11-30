6bc6182e836219a63de0486c6188d81a8f41baf0
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-30 10:46:26 +0100

    MouseController.cs :  Added comments.

--- Assets/Scripts/Entities/Player/MouseController.cs
+++ Assets/Scripts/Entities/Player/MouseController.cs
@@ -1,8 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        // TODO: Look into getting the interactables only once instead, saving performance.
+    }
+
+    private void Update()
+    {
+        var nothing = 0;
+        
+        // Cast a ray.
+        
+        // Check if the hit object implements the IInteractable interface.
+        
+        // If true, Check if the player pressed the mouse button.
+        
+        // If they did, call OnClick on the hovered object, and OnHover if they didn't.
+    }
 }
