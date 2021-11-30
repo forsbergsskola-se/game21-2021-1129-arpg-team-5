f04b86ba89938582f251f961297cd8df3954284b
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-30 14:13:04 +0100

    Player.cs :  Cleanup

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : Entity
+namespace Entities.Player
 {
+    public class Player : Entity
+    {
+    }
 }
