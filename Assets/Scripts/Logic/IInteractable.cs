406e8a028849ef2c214f1fd9ea09197c42286c3b
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-30 10:17:44 +0100

    IInteractable.cs :  Added in preparation for mouse input.
    Added a interface for objects that will have OnClick and OnHover methods.

---
+++ Assets/Scripts/Logic/IInteractable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractabe
+{
+    /// <summary>
+    /// Implement your OnHover method. If the mouse sprite should change, access a sprite variable in the MouseControl script. LAST PART NOT IMPLEMENTED YET, MIGHT CHANGE!
+    /// </summary>
+    public void OnHover();
+
+    /// <summary>
+    /// Implement your OnClick method. This is called if your object is clicked, so implement any logic that should happen if a object is clicked here.
+    /// </summary>
+    public void OnClick();
+}
