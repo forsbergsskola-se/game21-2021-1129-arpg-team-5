91863ff2cc4401d6e1f0969347a99ce4243320d5
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-02 11:32:08 +0100

    GateLogic.cs: Added pretty much empty script to be worked on later.

---
+++ Assets/Personal Folders/Johannes/GateLogic.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateLogic : MonoBehaviour, IOpenLogic
+{
+    [SerializeField] private float MoveSpeed;
+    
+    // Method to open door:
+    public void Open()
+    {
+        
+    }
+}
