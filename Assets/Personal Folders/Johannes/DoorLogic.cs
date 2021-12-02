3e73b37a2da27383889feb06ba1e2d3c19a047ca
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-02 16:27:53 +0100

    DoorLogic.cs:  Updated to make more friendly for designers to balance.

--- Assets/Personal Folders/Johannes/DoorLogic.cs
+++ Assets/Personal Folders/Johannes/DoorLogic.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class DoorLogic : MonoBehaviour, IOpenLogic
 {
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float timeToOpen;
+
 
     private bool isOpen;
-    
-    
+    private float movementPerFrame;
+    private float totalAnimationFrames;
+
+    private void Awake()
+    {
+        totalAnimationFrames = Mathf.Round(timeToOpen * 60);
+        movementPerFrame = 90 / totalAnimationFrames;
+    }
+
+
     public void Open()
     {
         if (isOpen) 
@@ -21,12 +32,18 @@ public class DoorLogic : MonoBehaviour, IOpenLogic
 
     private IEnumerator OpeningAnimation()
     {
-        Debug.Log("Hello");
+        // Debug.Log("Hello");
+        //
+        // for (int angle = 0; angle < 90; angle++)
+        // {
+        //     yield return new WaitForSeconds(0.01f);
+        //     transform.Rotate(Vector3.up, -1);
+        // }
 
-        for (int angle = 0; angle < 90; angle++)
+        for (int i = 0; i < totalAnimationFrames; i++)
         {
-            yield return new WaitForSeconds(0.01f);
-            transform.Rotate(Vector3.up, -1);
+            yield return new WaitForSeconds(0.0166f);
+            transform.Rotate(Vector3.up, movementPerFrame);
         }
         
     }
