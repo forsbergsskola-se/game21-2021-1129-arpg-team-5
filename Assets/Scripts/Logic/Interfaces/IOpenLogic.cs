6aa5358fa0957e5ecea570fbfb7bec8e72bb4d54
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-02 11:27:15 +0100

    IOpenLogic.cs: Created.
    Interface for door opening logic, since we might have different types of doors/gates. And want to do different things after opening them.

---
+++ Assets/Scripts/Logic/Interfaces/IOpenLogic.cs
@@ -0,0 +1,7 @@
+public interface IOpenLogic
+{
+    /// <summary>
+    /// Call this to run the open animation of the object and possibly other logic after.
+    /// </summary>
+    public void Open();
+}
