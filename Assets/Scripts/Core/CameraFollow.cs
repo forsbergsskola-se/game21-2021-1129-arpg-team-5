f1ce1968eade9f477ef06e5e03b73b4840bffc66
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2021-12-06 14:43:41 +0100

    Update CameraFollow.cs

--- Assets/Scripts/Core/CameraFollow.cs
+++ Assets/Scripts/Core/CameraFollow.cs
@@ -8,11 +8,9 @@ namespace Team5.Core
     {
         [SerializeField]
         private Transform target;
-
         private void LateUpdate()
         {
             transform.position = target.position;
         }
-
     }
 }
