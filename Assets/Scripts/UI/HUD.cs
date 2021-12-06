fc8064502ff205cdae4091595c1b8a2611d1e601
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-12-06 16:43:35 +0100

    Added HUD.cs

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public TMP_Text ScoreText;
+    public TMP_Text ReviveText;
+    public TMP_Text HealthText;
+    public TMP_Text CashText;
+}
