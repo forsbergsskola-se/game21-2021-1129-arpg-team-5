839fb1d2ed7e0770c2e6084931c10546c41c5844
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-12-07 12:37:30 +0100

    New feature: Enemy Take Damage Indicator

--- Assets/Scripts/UI/EnemyHealth.cs
+++ Assets/Scripts/UI/EnemyHealth.cs
@@ -4,23 +4,39 @@ using System.Collections.Generic;
 using Team5.Core;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class EnemyHealth : MonoBehaviour
 {
     private Health healthStats;
+    private float maxHealth;
     private float health;
-    private TMP_Text healthText;
+    private float damage;
+    private float hurtHealth;
+    [SerializeField]
+    public TMP_Text healthTextIndicator;
+    [SerializeField]
+    public TMP_Text hurtHealthTextIndicator;
+
 
     private void Start()
     {
         healthStats = this.GetComponent<Health>();
-        healthText = this.GetComponentInChildren<TMP_Text>();
+        maxHealth = healthStats.maxHealth;
     }
 
 
     void Update()
     {
         health = healthStats.healthPoint;
-        healthText.text = "" + health;
+        healthTextIndicator.text = "" + health;
+        hurtHealthTextIndicator.text = "";
+
+        if (health < maxHealth)
+        {
+            damage = healthStats.damageAmount;
+            hurtHealth = damage;
+            hurtHealthTextIndicator.text = "-" + hurtHealth;
+        }
     }
 }
