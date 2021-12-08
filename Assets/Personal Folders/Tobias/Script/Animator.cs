c31f98a4b0b156cbcb4b6d45b6977e9b3fb33cc8
AlchemistWolf <tobias_lundin@yahoo.com>
2021-12-08 15:57:13 +0100

    vase destroyed

---
+++ Assets/Personal Folders/Tobias/Script/Animator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+    private Animator m_Animator;
+
+    void start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if 
+    }
+}
