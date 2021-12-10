a4f603c9054d0e9557b4fa3f3193f39a521ba297
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-10 10:06:59 +0100

    GateLogic.cs: Cleanup and move to Team5.World.Interactables namespace.

--- Assets/Scripts/World/GateLogic.cs
+++ Assets/Scripts/World/GateLogic.cs
@@ -1,40 +1,49 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Team5.Core;
 
-public class GateLogic : MonoBehaviour, IOpenLogic
+namespace Team5.World.Interactables
 {
-    [SerializeField] private float timeToGoUpp;
-    [SerializeField] private float distanceToOpen;
-    private float totalAntationFrames;
-    private const float AnimationFramerate = 60;
-    private float frameTime;
-    private bool isOpen;
-    private float movementUpPerFrame;
+    public class GateLogic : MonoBehaviour, IOpenLogic
+    {
+        [SerializeField] private float distanceToOpen;
+        [SerializeField] private float timeToGoUp;
     
-    private void Awake()
-    {
-        totalAntationFrames = Mathf.Round(timeToGoUpp * AnimationFramerate);
-        movementUpPerFrame = distanceToOpen / totalAntationFrames;
-        frameTime = 1 / AnimationFramerate;
-    }
-
-    // Method to open door:
-    public void Open()
-    {
-        if (isOpen)
-            return;
-        StartCoroutine(OpeningAnimation());
-    }
-
-    private IEnumerator OpeningAnimation()
-    {
-        for (int distance = 0; distance < totalAntationFrames; distance++)
+    
+        private const float AnimationFramerate = 60;
+        private float frameTime;
+        private bool isOpen;
+        private float movementUpPerFrame;
+        private float totalAnimationFrames;
+    
+    
+    
+        private void Awake()
         {
-            yield return new WaitForSeconds(frameTime);
-            transform.Translate(movementUpPerFrame*Vector3.up);
+            totalAnimationFrames = Mathf.Round(timeToGoUp * AnimationFramerate);
+            movementUpPerFrame = distanceToOpen / totalAnimationFrames;
+            frameTime = 1 / AnimationFramerate;
+        }
+
+    
+    
+        // Method to open door:
+        public void Open()
+        {
+            if (isOpen)
+                return;
+            StartCoroutine(OpeningAnimation());
+        }
+
+    
+    
+        private IEnumerator OpeningAnimation()
+        {
+            for (int distance = 0; distance < totalAnimationFrames; distance++)
+            {
+                yield return new WaitForSeconds(frameTime);
+                transform.Translate(movementUpPerFrame*Vector3.up);
+            }
         }
     }
 }
