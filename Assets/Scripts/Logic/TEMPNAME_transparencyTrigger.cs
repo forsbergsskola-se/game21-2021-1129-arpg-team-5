92ecfee4edf862aecdf1941de1755a1cad112470
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-10 13:33:05 +0100

    TEMPNAME_transparencyTrigger.cs: Added empty script.
    Will be used to test ways of making doors and pillars transparent when a player or enemy is behind them.

---
+++ Assets/Scripts/Logic/TEMPNAME_transparencyTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEMPNAME_transparencyTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Shoot a ray toward the camera.
+        
+        // If the ray hits a object
+    }
+}
