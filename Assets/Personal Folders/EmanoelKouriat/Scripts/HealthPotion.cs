958bf2258a5eb4395df822dd8806014e04f331d8
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2021-12-14 14:11:38 +0100

    HealthPotion.cs: Item heals hp to the player

--- Assets/Personal Folders/EmanoelKouriat/Scripts/HealthPotion.cs
+++ Assets/Personal Folders/EmanoelKouriat/Scripts/HealthPotion.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using Team5.Entities.Player;
 
 namespace team5.Inventory.Items.Consumables
 {
     public class HealthPotion : MonoBehaviour, IConsumable
     {
+        private GameObject player;
 
+        private void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
         public void Consume()
         {
-            Debug.Log("this");
+            
+            //TODO: Potion gives max hp + 10
+            player.GetComponent<PlayerController>().AddHealth(50);
+            Debug.Log("+50 HP");
         }
     }
 }
