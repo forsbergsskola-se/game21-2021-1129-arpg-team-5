7d423f2354e0ca3e431a63e46c911121a8a9546f
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-14 17:23:56 +0100

    Shackle.cs: Prepared for future functions

--- Assets/Personal Folders/EmanoelKouriat/Scripts/Shackle.cs
+++ Assets/Personal Folders/EmanoelKouriat/Scripts/Shackle.cs
@@ -5,14 +5,21 @@ namespace Team5.Inventory.Items.Accessories
 {
     public class Shackle : MonoBehaviour, IAccessory
     {
+        public bool Equipped;
+
+        [SerializeField] private int Armor;
+
 
         public void Equip()
         {
-            
+            Debug.Log("Equiped!");
         }
+        
+        
+        
         public void UnEquip()
         {
-            
+            Debug.Log("Unequiped!");
         }
     }
 }
