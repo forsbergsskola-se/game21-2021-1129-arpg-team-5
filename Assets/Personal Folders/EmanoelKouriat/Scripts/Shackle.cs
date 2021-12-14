24f49fd67ee4271e7787314101f66c27e1bcbfd9
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2021-12-14 16:02:23 +0100

    Update Shackle.cs

--- Assets/Personal Folders/EmanoelKouriat/Scripts/Shackle.cs
+++ Assets/Personal Folders/EmanoelKouriat/Scripts/Shackle.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Team5.Entities.Player;
 
 namespace Team5.Inventory.Items.Equipables
 {
@@ -7,11 +8,11 @@ namespace Team5.Inventory.Items.Equipables
 
         public void Equip()
         {
-            throw new System.NotImplementedException();
+            
         }
         public void UnEquip()
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
