22e81d416dae9a9abb944884cf5c0ad8c8f121ab
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-14 17:23:26 +0100

    InventoryTemp.cs: Expermiental work

--- Assets/Personal Folders/EmanoelKouriat/Scripts/InventoryTemp.cs
+++ Assets/Personal Folders/EmanoelKouriat/Scripts/InventoryTemp.cs
@@ -11,10 +11,16 @@ public class InventoryTemp : MonoBehaviour
     private ConsumableController consumableController;
     public GameObject itemPrefab;
 
-    private KeyCode[] keyInput =
+    public List<GameObject> accessorySlots = new();
+    
+    private KeyCode[] keyInput1 =
     {
-        KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.T, KeyCode.Y,
-        KeyCode.U, KeyCode.I, KeyCode.O, KeyCode.P
+        KeyCode.Q, KeyCode.W, KeyCode.E,
+    };
+    
+    private KeyCode[] keyInput2 =
+    {
+        KeyCode.R, KeyCode.T, KeyCode.Y, KeyCode.U
     };
 
     private void Start()
@@ -23,15 +29,32 @@ public class InventoryTemp : MonoBehaviour
         item = Instantiate(itemPrefab);
         item.transform.SetParent(this.transform);
         item.transform.position = this.transform.position;
+        
+        
+        
+        // Get all the hotbar slots and put them inside a list.
+        Transform slotSearch = gameObject.transform;
+        for (int i = 1; slotSearch != null; i++)
+        {
+            slotSearch = transform.Find($"AccessorySlot{i}");
+            Debug.Log(slotSearch);
+            if (slotSearch != null)
+                accessorySlots.Add(slotSearch.gameObject);
+        }
+        
+        foreach (var VARIABLE in accessorySlots)
+        {
+            Debug.Log("Found: " + VARIABLE.name);
+        }
     }
 
     private void Update()
     {
-        for (int i = 0; i < keyInput.Length; i++)
+        for (int i = 0; i < keyInput1.Length; i++)
         {
-            if (Input.GetKeyDown(keyInput[i]))
+            if (Input.GetKeyDown(keyInput1[i]))
             {
-                Debug.Log($"Clicked hehe {keyInput[i]}");
+                Debug.Log($"Clicked hehe {keyInput1[i]}");
                 
                 if (consumableController.hotbarSlots[i] != null)
                 {
@@ -43,6 +66,28 @@ public class InventoryTemp : MonoBehaviour
                     
                     item.transform.position = consumableController.hotbarSlots[i].transform.position;
                     item.transform.SetParent(consumableController.hotbarSlots[i].transform);
+                }
+            }
+        }
+        
+        for (int i = 0; i < keyInput2.Length; i++)
+        {
+            if (Input.GetKeyDown(keyInput2[i]))
+            {
+                Debug.Log($"Clicked hehe {keyInput2[i]}");
+                
+                if (accessorySlots[i] != null)
+                {
+                    if (item.IsDestroyed())
+                    {
+                        Instantiate(itemPrefab);
+                        item = GameObject.Find("Item(Clone)");
+                    }
+
+                    accessorySlots[i].GetComponent<AccessoryController>().Accessory = item;
+
+                    // item.transform.position = accessorySlots[i].transform.position;
+                    // item.transform.SetParent(accessorySlots[i].transform);
                 }
             }
         }
