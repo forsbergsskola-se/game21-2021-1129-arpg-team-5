8164fd9110ab5cc29d2653d86d0b11f04e806257
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2021-12-14 14:07:22 +0100

    PlayerController.cs: Add AddHealth Method

--- Assets/Scripts/Entities/Player/PlayerController.cs
+++ Assets/Scripts/Entities/Player/PlayerController.cs
@@ -113,9 +113,18 @@ namespace Team5.Entities.Player
             return false;
         }
 
+        
+        
         private static Ray GetMouseRay()
         {
             return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+
+
+
+        public void AddHealth(int value)
+        {
+            Health += value;
         }
     }
 }
