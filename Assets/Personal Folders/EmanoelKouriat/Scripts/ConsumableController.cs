da5b48f7eb6e9900f60d265ec24818b0d0d53a9b
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-14 17:23:11 +0100

    ConsumableController.cs: Removed unused keycodes

--- Assets/Personal Folders/EmanoelKouriat/Scripts/ConsumableController.cs
+++ Assets/Personal Folders/EmanoelKouriat/Scripts/ConsumableController.cs
@@ -10,8 +10,7 @@ namespace Team5.Ui.Hotbar
         
         private KeyCode[] keyInput =
         {
-            KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5,
-            KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0
+            KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3
         };
 
         private void Start()
