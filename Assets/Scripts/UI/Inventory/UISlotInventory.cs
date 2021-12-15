9ab9ef7ed8a3357a0a02673abf6cfae4e3c9b0a4
toofantah <toofan.tah@gmail.com>
2021-12-15 12:22:59 +0300

    UISlotInventory.cs

--- Assets/Scripts/UI/Inventory/UISlotInventory.cs
+++ Assets/Scripts/UI/Inventory/UISlotInventory.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Team5.Ui.Inventories;
+using Team5.Ui.Drag;
 
 namespace Team5.Ui
 {
-    public class UISlotInventory : MonoBehaviour  
+    public class UISlotInventory : MonoBehaviour , IDragableContainer<Sprite>
     {
+        [SerializeField] ItemIconInventory icon = null;
+        public void AddItems(Sprite item, int number)
+        {
+            icon.SetItem(item);
+        }
+
+        public Sprite GetItem()
+        {
+            return icon.GetItem();
+        }
+
+        public int GetNumber()
+        {
+            return 1;
+        }
+
+        public int MaxAcceptable(Sprite item)
+        {
+            if (GetItem() == null)
+            {
+                return int.MaxValue;
+            }
+            return 0;
+        }
+
+        public void RemoveItem(int number)
+        {
+            icon.SetItem(null);
+        }
+
         // Start is called before the first frame update
         void Start()
         {
