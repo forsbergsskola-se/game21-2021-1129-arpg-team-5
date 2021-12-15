5fc19a2618293989ae66eeb53e14a1e8c5face87
toofantah <toofan.tah@gmail.com>
2021-12-15 11:56:47 +0300

    Update IDrag.cs
    Components that implement this interfaces can act as the source for dragging a `DragItem`.

--- Assets/Scripts/UI/Interfaces/IDrag.cs
+++ Assets/Scripts/UI/Interfaces/IDrag.cs
@@ -6,8 +6,13 @@ namespace Team5.Ui.Drag
 {
     public interface IDrag<T> where T: class 
     {
-        T GetItem(); //What item type currently resides in this source
-        int GetNumber(); //What is the quantity of items in this source
-        void RemoveItem(int number); // Remove a given number of items from the source and never should exceed the number that retursn by GetNumber();
+        //What item type currently resides in this source
+        T GetItem(); 
+
+        //What is the quantity of items in this source
+        int GetNumber(); 
+
+        // Remove a given number of items from the source and never should exceed the number that retursn by GetNumber();
+        void RemoveItem(int number); 
     }
 }
