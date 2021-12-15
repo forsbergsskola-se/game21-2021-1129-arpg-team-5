abaaf52723a8c5e7812a88e517cfa1501c4a6f05
toofantah <toofan.tah@gmail.com>
2021-12-15 12:19:19 +0300

    ItemIconInventory.cs
    it sets and gets the sprites for slots

---
+++ Assets/Scripts/UI/Inventory/ItemIconInventory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Team5.Ui.Inventories
+{
+    [RequireComponent(typeof(Image))]
+    public class ItemIconInventory : MonoBehaviour
+    {
+        public void SetItem(Sprite item)
+        {
+            var iconImage = GetComponent<Image>();
+            if (item == null)
+            {
+                iconImage.enabled = false;
+            }
+            else
+            {
+                iconImage.enabled = true;
+                iconImage.sprite = item;
+            }
+        }
+
+        public Sprite GetItem()
+        {
+            var iconImage = GetComponent<Image>();
+            if (!iconImage.enabled)
+            {
+                return null;
+            }
+            return iconImage.sprite;
+        }
+    }
+}
