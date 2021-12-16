925700dc0a5c617350665974149fcf8e12a88158
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-16 14:59:51 +0100

    Box.cs: Updated interface implementation.

--- Assets/Personal Folders/Oskar/Scripts/Box.cs
+++ Assets/Personal Folders/Oskar/Scripts/Box.cs
@@ -7,10 +7,15 @@ namespace Personal_Folders.Oskar.Scripts
     {
         public Texture2D mouseTexture => cursorTexture;
         public Texture2D cursorTexture;
-        public void OnHover()
+        public void OnHoverEnter()
         {
             Debug.Log("I hover over the Box");
         }
+
+        public void OnHoverExit()
+        {
+        }
+
         public void OnClick(Vector3 mouseClickVector)
         {
             Debug.Log("I clicked on the Box");
