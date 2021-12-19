540cba7a039b8d43e8587c33e5480c2bef2819e3
toofantah <toofan.tah@gmail.com>
2021-12-19 18:32:44 +0300

    Save System Test

--- Assets/Scripts/Inventory/Saving/SaveableEntity.cs
+++ Assets/Scripts/Inventory/Saving/SaveableEntity.cs
@@ -44,39 +44,10 @@ namespace Team5.Saving
         /// </summary>
         /// <param name="state">
         /// The same object that was returned by `CaptureState`.
-        /// </param>
-        public void RestoreState(object state)
-        {
-            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
-            foreach (ISaveable saveable in GetComponents<ISaveable>())
-            {
-                string typeString = saveable.GetType().ToString();
-                if (stateDict.ContainsKey(typeString))
-                {
-                    saveable.RestoreState(stateDict[typeString]);
-                }
-            }
-        }
-
+       
 
 #if UNITY_EDITOR
 
-        private void Update() {
-            if (Application.IsPlaying(gameObject)) return;
-            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
-
-            SerializedObject serializedObject = new SerializedObject(this);
-            SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
-            
-            if (string.IsNullOrEmpty(property.stringValue) || !IsUnique(property.stringValue))
-            {
-                property.stringValue = System.Guid.NewGuid().ToString();
-                serializedObject.ApplyModifiedProperties();
-            }
-
-            globalLookup[property.stringValue] = this;
-        }
-#endif
 
         private bool IsUnique(string candidate)
         {
