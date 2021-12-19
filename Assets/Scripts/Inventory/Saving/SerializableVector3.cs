3feefdb552f6a1d8a75fd70eda298faf21edbda1
toofantah <toofan.tah@gmail.com>
2021-12-19 17:56:47 +0300

    Reorganization of nameSpaces and scripts

---
+++ Assets/Scripts/Inventory/Saving/SerializableVector3.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Team5.Saving
+{
+    /// <summary>
+    /// A System.Serializable wrapper for the Vector3 class.
+    /// </summary>
+    [System.Serializable]
+    public class SerializableVector3
+    {
+        float x;
+        float y;
+        float z;
+
+        /// <summary>
+        /// Copy over the state of an existing Vector3.
+        /// </summary>
+        public SerializableVector3(Vector3 vector)
+        {
+            x = vector.x;
+            y = vector.y;
+            z = vector.z;
+        }
+
+        /// <summary>
+        /// Create a Vector3 from this class state.
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 ToVector()
+        {
+            return new Vector3(x, y, z);
+        }
+    }
+}
