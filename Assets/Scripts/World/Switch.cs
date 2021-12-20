69519f816699416132023fcb83c6f0507394bfa1
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-20 10:37:18 +0100

    Switch.cs: Interactable switch that unlocks door.
    - Added: Interactable switch.
    - Added: Turns of box collider and script itself after activation.
    - Added: Moves the lever down to indicate it has been used after clicked.
    - Added: Temporary mouse cursor. Change later.

---
+++ Assets/Scripts/World/Switch.cs
@@ -0,0 +1,39 @@
+using Team5.Core;
+using UnityEngine;
+
+namespace Team5.World.Interactables
+{
+    public class Switch : MonoBehaviour, IInteractable
+    {
+        public InteractableBarrierController barrier;
+        [SerializeField] private Texture2D OnHoverMousetexture;
+
+        private void Start()
+        {
+            if (barrier == null)
+            {
+                Debug.Log("Switch has no assigned InteractableBarrierController!");
+            }
+            barrier.isLocked = true;
+        }
+
+        public Texture2D mouseTexture => OnHoverMousetexture;
+        public void OnHoverEnter()
+        {
+        }
+
+        public void OnHoverExit()
+        {
+        }
+
+        public void OnClick(Vector3 mouseClickVector)
+        {
+            barrier.isLocked = false;
+            
+            transform.Find("Lever").transform.Rotate(Vector3.back, 60);
+            
+            GetComponent<BoxCollider>().enabled = false;
+            this.enabled = false;
+        }
+    }
+}
