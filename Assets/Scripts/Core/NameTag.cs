5fa8300bee5687bc83b3a090213beb3473575a1d
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-12-20 15:51:31 +0100

    NameTag.cs now has an automated naming function
    Uses this.gameobject name to feed TMP string

--- Assets/Scripts/Core/NameTag.cs
+++ Assets/Scripts/Core/NameTag.cs
@@ -3,36 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Team5.Core;
+using TMPro;
 
 namespace Team5.Core
 {
     public class NameTag : MonoBehaviour, IInteractable
     {
         private GameObject Nametag;
+        private TextMeshPro name;
         [SerializeField] private Texture2D Cursor;
         public Texture2D mouseTexture => Cursor;
 
         public void Awake()
         {
             Nametag = this.gameObject.transform.Find("Name Tag").gameObject;
+            name = Nametag.GetComponent<TextMeshPro>();
+            name.text = $"{this.gameObject.name}";
         }
 
         public void OnHoverEnter()
         {
             Nametag.SetActive(true);
-            Debug.Log($"mouse over {this.name}");
+            Debug.Log($"mouse hover over {name.text}");
         }
         
         public void OnHoverExit()
         {
             Nametag.SetActive(false);
-            Debug.Log($"mouse off {this.name}");
+            Debug.Log($"mouse off {name.text}");
         }
 
       
         public void OnClick(Vector3 mouseClickVector)
         {
-            // click and something happens
+            Nametag.SetActive(false);
         }
     }
 }
