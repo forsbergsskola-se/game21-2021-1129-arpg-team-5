37ad62b1b8b6c03b0b36ba92c9472da71ca1f1bf
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-12-21 11:06:38 +0100

    HUD.cs new method for disabling/ enabling HUD elements

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 
@@ -6,14 +7,35 @@ namespace Team5.Ui
 {
     public class HUD : MonoBehaviour
     {
+        // HUD UI
         public TMP_Text ScoreText;
         public TMP_Text ReviveText;
         public TMP_Text HealthText;
         public TMP_Text CashText;
         public TMP_Text KillCountText;
         public TMP_Text LvlText;
+        public GameObject SkullCounter;
+        public GameObject InventoryMenu;
+        
+        // Shop UI
         public GameObject ShopText;
         public TMP_Text ShopDialogue;
         public GameObject SkullIcons;
+        public GameObject DialogueHeadMain;
+        public GameObject DialogueHeadNPC;
+
+        
+        public void HudUIActive(bool Health, bool Level, bool Score, bool Revives, 
+                                bool KillCount, bool Inventory, bool Skulls)
+        {
+            HealthText.enabled = Health;
+            LvlText.enabled = Level;
+            ScoreText.enabled = Score;
+            ReviveText.enabled = Revives;
+            KillCountText.enabled = KillCount;
+            InventoryMenu.SetActive(Inventory);
+            SkullCounter.SetActive(Skulls);
+        }
     }
+
 } 
