1d0bac5972a8e24a0f838f2c181447ef344a1b8c
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-12-27 12:26:59 +0100

    SceneController.cs - pause and resume game logic added

--- Assets/Scripts/UI/SceneController.cs
+++ Assets/Scripts/UI/SceneController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +9,8 @@ namespace Team5.Ui
 {
     public class SceneController : MonoBehaviour
     {
+        private GameObject pauseMenu;
+
         public void LoadScene(string sceneName)
         {
             SceneManager.LoadScene(sceneName);
@@ -17,6 +21,28 @@ namespace Team5.Ui
         {
             Application.Quit();
             Debug.Log("Quit Button: Pressed");
+        }
+
+        public void PauseGame()
+        {
+            Time.timeScale = 0;
+            Debug.Log("Game Paused");
+        }
+
+        public void ResumeGame()
+        {
+            pauseMenu = GameObject.FindWithTag("Pause Menu");
+            if (pauseMenu.activeInHierarchy)
+            {
+                pauseMenu.SetActive(false);
+            }
+            Time.timeScale = 1;
+            Debug.Log("Game Resumed");
+        }
+        
+        public void MuteUnmuteBus(bool setMuted){
+
+            //masterBus.setMute(setMuted);
         }
     }   
 }
