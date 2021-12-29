ac12d98af3852398ee105669a78beccbd2e9da1c
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-29 13:53:09 +0100

    RoomController.cs: Add method to disable all objects inside field array.

--- Assets/Personal Folders/Oskar/RoomController.cs
+++ Assets/Personal Folders/Oskar/RoomController.cs
@@ -2,4 +2,20 @@ using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
+    
+    [Tooltip("Add objects that should be hidden when the room is unlocked.")]
+    public GameObject[] objectsToDisable;
+    
+
+    
+    /// <summary>
+    /// Disable all selected objects to make the room more open and clear..
+    /// </summary>
+    public void DisableObjectsForVisibility()
+    {
+        foreach (var obj in objectsToDisable)
+        {
+            obj.SetActive(false);
+        }
+    }
 }
