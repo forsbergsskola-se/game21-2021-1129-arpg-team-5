18b52b60660ae193916f7ee1c050ebb5478feea3
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-12-29 16:31:23 +0100

    SliderVolume.cs: Trying to implement a mute toggle

--- Assets/Scripts/UI/SliderVolume.cs
+++ Assets/Scripts/UI/SliderVolume.cs
@@ -3,39 +3,73 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Slider = UnityEngine.UI.Slider;
 
 namespace Team5.Ui
 {
     public class SliderVolume : MonoBehaviour
     {
-        public TMP_Text volume;
+        private TMP_Text volumeText;
+        public GameObject slider;
+        
         private float sliderValue;
         private float sliderVolume;
         private float mute = 0;
-
+        private bool muteToggle = false;
+        private float oldSliderValue;
+        
         string masterBusString = "Bus:/";
         FMOD.Studio.Bus masterBus;
 
         private void Awake()
         {
             masterBus = FMODUnity.RuntimeManager.GetBus(masterBusString);
+            volumeText = slider.GetComponentInChildren<TMP_Text>();
         }
         
         void Update()
         {
+
             // access Slider component's value
-            sliderValue = this.gameObject.GetComponent<Slider>().value;
+            sliderValue = slider.GetComponent<Slider>().value;
             
             // times by 100 to get percentage number as normally between 0-1
             sliderVolume = sliderValue * 100;
         
             // round to int when printing to get nice full number
-            volume.SetText(Mathf.RoundToInt(sliderVolume) + "");
+            volumeText.SetText(Mathf.RoundToInt(sliderVolume) + "");
             
             // apply to master volume
             masterBus.setVolume(sliderValue);
             masterBus.getVolume(out sliderValue);
+        }
+
+        public void MuteEverythingToggle()
+        {
+            if (muteToggle == false)
+            {
+                MuteEverything();
+            }
+
+            else
+            {
+                UnMute();
+            }
+        }
+        
+        public void MuteEverything()
+        {
+            oldSliderValue = sliderValue;
+            sliderValue = mute;
+            muteToggle = true;
+
+        }
+
+        public void UnMute()
+        {
+            sliderValue = oldSliderValue;
+            muteToggle = false;
         }
     }
 }
