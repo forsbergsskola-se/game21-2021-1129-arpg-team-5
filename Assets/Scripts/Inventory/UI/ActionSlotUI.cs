d858f8a51a0f0c41ec5c937d7ffe3a616088ca7e
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-03 13:44:42 +0100

    ActionSlotUI.cs: Now handles user input for using items.

--- Assets/Scripts/Inventory/UI/ActionSlotUI.cs
+++ Assets/Scripts/Inventory/UI/ActionSlotUI.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Team5.Core.UI.Dragging;
 using Team5.Inventories;
 using Team5.UI.Inventories;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 namespace Team5.UI.Inventories
@@ -17,6 +20,7 @@ namespace Team5.UI.Inventories
         public void AddItems(InventoryItem item, int number)
         {
             store.AddAction(item, index, number);
+            Debug.Log($"Added {item.name} at index {index} with amount {number}");
         }
 
         public InventoryItem GetItem()
@@ -48,6 +52,28 @@ namespace Team5.UI.Inventories
         void UpdateIcon()
         {
             icon.SetItem(GetItem(), GetNumber());
+        }
+
+
+        private const int NumberKeysOffset = 49;
+        
+        private void Update()
+        {
+            Debug.Log("Testing");
+            if (Input.GetKeyDown((KeyCode)(NumberKeysOffset+index)))
+            {
+                Debug.Log("SMÖRGÅS nr " + index);
+
+                Debug.Log($"Object: {store.name}");
+
+                // if (GetItem().TryGetComponent(out IConsumable consumable))
+                // {
+                //     Debug.Log("EatEateateat nomnomnom");
+                //     consumable.Consume();
+                // }
+
+                store.Use(index, store.gameObject);
+            }
         }
     }
 }
