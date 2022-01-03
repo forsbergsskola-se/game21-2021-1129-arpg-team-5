8863b4b7da1dd219edb50d4f8b6eea0150ff19c3
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-03 17:31:47 +0100

    EquippableStatModifier.cs: Working solution that gives armor.

---
+++ Assets/Scripts/Items/EquippableStatModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Team5.Entities;
+using UnityEngine;
+
+namespace Team5.Inventories.Items
+{
+    public class EquippableStatModifier : MonoBehaviour, IEquippable
+    {
+        public void Equip()
+        {
+            Debug.Log("Equipped " + name);
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Entity>().Armor += 20;
+        }
+
+        public void UnEquip()
+        {
+            Debug.Log("UnEquipped " + name);
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Entity>().Armor -= 20;
+        }
+    }
+}
