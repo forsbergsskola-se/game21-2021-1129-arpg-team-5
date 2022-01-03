6d85252921ff0d68d56e9ee67ec2bb73d54e0304
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-03 15:36:00 +0100

    ActionSlot.cs: Cleanup.
    Removal of unused code and comments.

--- Assets/Scripts/Inventory/UI/ActionSlotUI.cs
+++ Assets/Scripts/Inventory/UI/ActionSlotUI.cs
@@ -1,21 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Team5.Core.UI.Dragging;
 using Team5.Inventories;
-using Team5.UI.Inventories;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 namespace Team5.UI.Inventories
 {
     public class ActionSlotUI : MonoBehaviour, IItemHolder, IDragContainer<InventoryItem>
     {
-        [SerializeField] InventoryItemIcon icon = null;
-        [SerializeField] int index = 0;
+        [SerializeField] private InventoryItemIcon icon = null;
+        [SerializeField] private int index = 0;
 
-        ActionStore store;
+        private ActionStore store;
+        private const int NumberKeysOffset = 49;
+        
+        
 
         public void AddItems(InventoryItem item, int number)
         {
@@ -27,12 +24,12 @@ namespace Team5.UI.Inventories
         {
             return store.GetAction(index);
         }
-
+        
         public int GetNumber()
         {
             return store.GetNumber(index);
         }
-
+        
         public int MaxAcceptable(InventoryItem item)
         {
             return store.MaxAcceptable(item, index);
@@ -54,26 +51,10 @@ namespace Team5.UI.Inventories
             icon.SetItem(GetItem(), GetNumber());
         }
 
-
-        private const int NumberKeysOffset = 49;
-        
         private void Update()
         {
-            Debug.Log("Testing");
             if (Input.GetKeyDown((KeyCode)(NumberKeysOffset+index)))
-            {
-                Debug.Log("SMÖRGÅS nr " + index);
-
-                Debug.Log($"Object: {store.name}");
-
-                // if (GetItem().TryGetComponent(out IConsumable consumable))
-                // {
-                //     Debug.Log("EatEateateat nomnomnom");
-                //     consumable.Consume();
-                // }
-
                 store.Use(index, store.gameObject);
-            }
         }
     }
 }
