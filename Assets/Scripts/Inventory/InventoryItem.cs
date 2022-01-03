2c164688db9c48efb3400fd5ac9b34db077bc7b6
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-03 17:32:56 +0100

    InventoryItem.cs: Made pickup public.
    Not something I wanted to do, but with the time we have it is better to get it working than to have it be the best possible way.

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -24,7 +24,7 @@ namespace Team5.Inventories
         [Tooltip("The UI icon to represent this item in the inventory.")]
         [SerializeField] Sprite icon = null;
         [Tooltip("The prefab that should be spawned when this item is dropped.")]
-        [SerializeField] protected Pickup pickup = null;
+        [SerializeField] public Pickup pickup = null; // TODO: Didn't want to make this public, but with the time constraints we have to take the easy solution. :/
         [Tooltip("If true, multiple items of this type can be stacked in the same inventory slot.")]
         [SerializeField] bool stackable = false;
 
