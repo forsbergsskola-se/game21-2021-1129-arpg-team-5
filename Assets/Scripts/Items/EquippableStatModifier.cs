3a97c02e78cf2f3065fead5bbf73381fd176769a
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-03 18:09:24 +0100

    EquippableStatModifier.cs: Preparations for tomorrows improvements.

--- Assets/Scripts/Items/EquippableStatModifier.cs
+++ Assets/Scripts/Items/EquippableStatModifier.cs
@@ -6,10 +6,16 @@ namespace Team5.Inventories.Items
 {
     public class EquippableStatModifier : MonoBehaviour, IEquippable
     {
+        [SerializeField] private float MaxHealthBoost;
+        [SerializeField] private float MovementSpeedBoost;
+        [SerializeField] private float ArmorBoost;
+
         public void Equip()
         {
             Debug.Log("Equipped " + name);
             GameObject.FindGameObjectWithTag("Player").GetComponent<Entity>().Armor += 20;
+            
+            // TODO: Make a nice way to modify the stat of choice.
         }
 
         public void UnEquip()
