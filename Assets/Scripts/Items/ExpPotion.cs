9cb67a298832e26b3e1aef5fa3687251419cdd76
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-04 10:09:14 +0100

    ExpPotion.cs: Added for easy level testing.

---
+++ Assets/Scripts/Items/ExpPotion.cs
@@ -0,0 +1,17 @@
+﻿using Team5.Entities;
+using UnityEngine;
+
+namespace Team5.Inventories.Items.Consumables
+{
+    public class ExpPotion : MonoBehaviour, IConsumable
+    {
+        [SerializeField] private int LevelsToAdd;
+        
+        public bool Consume(GameObject user)
+        {
+            user.GetComponent<Entity>().EntityLevel += LevelsToAdd;
+
+            return true;
+        }
+    }
+}
