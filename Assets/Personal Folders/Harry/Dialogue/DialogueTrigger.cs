f06dd68ad198171b032ee01ebb7bc50835f751c2
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-06 20:03:04 +0100

    DialogueTrigger.cs: Now updated to change to change npc image too

--- Assets/Personal Folders/Harry/Dialogue/DialogueTrigger.cs
+++ Assets/Personal Folders/Harry/Dialogue/DialogueTrigger.cs
@@ -1,14 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Team5.Ui;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour {
 
     public Dialogue dialogue;
+    private Image head;
 
     public void TriggerDialogue ()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        head = FindObjectOfType<HUD>().DialogueHeadNPC;
     }
+    
+    public bool CrowBlack = false;
+    public bool CrowWhite = false;
+    public bool EnemyGreen = false;
+    public bool EnemyPurple = false;
+    public bool EnemyRed = false;
+    public bool EnemyYellow = false;
+    
+    public Sprite CrowBlackSprite;
+    public Sprite CrowWhiteSprite;
+    public Sprite EnemyGreenSprite;
+    public Sprite EnemyPurpleSprite;
+    public Sprite EnemyRedSprite;
+    public Sprite EnemyYellowSprite;
+    
+    // Assigns character dialogue head sprite and disables others
+    private void Update()
+    {
+        if (CrowBlack == true)
+        {
+            head.sprite = CrowBlackSprite;
+            Disable(true,false,false,false,false,false);
+        }
+        
+        if (CrowWhite == true)
+        {
+            head.sprite = CrowWhiteSprite;
+            Disable(false,true,false,false,false,false);
 
+        }
+        
+        if (EnemyGreen == true)
+        {
+            head.sprite = EnemyGreenSprite;
+            Disable(false,false,true,false,false,false);
+
+        }
+        
+        if (EnemyPurple == true)
+        {
+            head.sprite = EnemyPurpleSprite;
+            Disable(false,false,false,true,false,false);
+
+        }
+        
+        if (EnemyRed == true)
+        {
+            head.sprite = EnemyRedSprite;
+            Disable(false,false,false,false,true,false);
+
+        }
+        
+        if (EnemyYellow == true)
+        {
+            head.sprite = EnemyYellowSprite;
+            Disable(false,false,false,false,false,true);
+
+        }
+        
+    }
+    
+    // mass enable/ disable method
+    
+    private void Disable(bool one, bool two, bool three, bool four, bool five, bool six)
+    {
+        CrowBlack = one;
+        CrowWhite = two;
+        EnemyGreen = three;
+        EnemyPurple = four;
+        EnemyRed = five;
+        EnemyYellow = six;
+    }
 }
