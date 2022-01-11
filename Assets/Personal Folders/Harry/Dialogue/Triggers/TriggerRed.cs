630a31d01349d01ae5d9ff20fd211bd546ccda5a
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-11 04:41:27 +0100

    New script: TriggerRed.cs

---
+++ Assets/Personal Folders/Harry/Dialogue/Triggers/TriggerRed.cs
@@ -0,0 +1,24 @@
+using Team5.Ui;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerRed : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+    private DialogueManager dialogueManager;
+    private Image head;
+    public Sprite sprite;
+
+    private void Awake()
+    {
+        head = FindObjectOfType<HUD>().DialogueHeadNPC.GetComponent<Image>();
+        dialogueManager = FindObjectOfType<DialogueManager>();
+    }
+
+    public void TriggerDialogue()
+    {
+        dialogueManager.StartDialogue(dialogue);
+        head.sprite = sprite;
+    }
+}
