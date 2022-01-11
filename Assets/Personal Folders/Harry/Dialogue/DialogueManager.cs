52906278deaeadaef8a81c16c2400fdf041abbbd
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-11 02:01:33 +0100

    DialogueManager.cs: FIXED and working

--- Assets/Personal Folders/Harry/Dialogue/DialogueManager.cs
+++ Assets/Personal Folders/Harry/Dialogue/DialogueManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Team5.Entities.Player;
 using Team5.Ui;
 using TMPro;
 using UnityEngine;
@@ -12,114 +11,64 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text nameText;
     public TMP_Text dialogueText;
     private TMP_Text button;
-    //private bool NPCDialogue;
-    //private bool KillQuest;
-    //private bool CollectQuest;
-
-    //private bool killQuestActive = false;
-    //private bool collectQuestActive;
-    //private bool reset = false;
-    //private int killTarget;
-    //private GameObject player;
-    private int playerKills;
+    private Image head;
+    public Sprite DialogueHead;
 
     public float slowTypeSpeed = 0.1f;
     public float mediumTypeSpeed = 0.05f;
     public float fastTypeSpeed = 0.03f;
     public float veryFastTypeSpeed = 0.01f;
-
-
-
+    
     public float typingSpeed { get; private set; } = 0.03f;
     private Queue<string> sentences;
 
     void Awake()
     {
-        //NPCDialogue = FindObjectOfType<DialogueTrigger>().NPCDialogue;
-        //KillQuest = FindObjectOfType<DialogueTrigger>().KillQuest;
-        //CollectQuest = FindObjectOfType<DialogueTrigger>().CollectQuest;
-        //killTarget = FindObjectOfType<DialogueTrigger>().KillTarget;
-        //player = GameObject.FindGameObjectWithTag("Player");
         typingSpeed = veryFastTypeSpeed;
-
-        
-        sentences = new Queue<string>();
         button = FindObjectOfType<HUD>().ContinueButton.GetComponentInChildren<TMP_Text>();
+        head = FindObjectOfType<HUD>().DialogueHeadNPC;
+        sentences = new Queue<string>();
     }
 
     public void StartDialogue (Dialogue dialogue)
     {
-        //killQuestActive = false;
-        //reset = false;
-        
+        dialogueText.text = "";
+        head.sprite = DialogueHead;
         nameText.text = dialogue.name;
         button.text = "Continue >>";
         sentences.Clear();
-
+        
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
+        
         DisplayNextSentence();
     }
     
 
-
-    public void DisplayNextSentence ()
+    public void DisplayNextSentence()
     {
         if (sentences.Count == 1)
         {
             FindObjectOfType<TalkingNPC>().ReplayOption();
             button.text = "Leave";
         }
-        
-        if (sentences.Count == 0)
+                
+        else if (sentences.Count == 0)
         {
             EndDialogue();
             return;
         }
         
-        /*if (sentences.Count == 1 && KillQuest == true)
-        {
-            killQuestActive = true;
-            FindObjectOfType<TalkingNPC>().ReplayOption();
-            button.text = "Leave";
-        }
-        
-        playerKills = player.GetComponent<PlayerController>().killCount;
-        string kills = $"You have killed {playerKills} out of {killTarget}";*/
-
-        StopAllCoroutines();
-
-        /*if (killQuestActive == true)
-        {
-            StartCoroutine(TypeSentence(kills));
-        }*/
-        
         string sentence = sentences.Dequeue(); 
+        StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
-
-  
-
 
     IEnumerator TypeSentence (string sentence)
     {
         dialogueText.text = "";
-        foreach (char letter in sentence.ToCharArray())
-        {
-            dialogueText.text += letter;
-            yield return new WaitForSecondsRealtime(typingSpeed);
-        }
-
-        /*if (killQuestActive)
-        {
-            reset = true;
-        }*/
-    }
-
-    IEnumerator KillDialogue(string sentence)
-    {
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
